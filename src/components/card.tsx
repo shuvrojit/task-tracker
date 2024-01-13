62918ae2ed3c177f51d7af15e049be8888355484
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-01-14 03:35:51 +0600

    Fix response issue

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -39,7 +39,7 @@ const Card = (data: CardData ) => {
         body: formData,
       });
 
-      // console.log('Upload successful', response);
+      console.log('Upload successful', response);
     } catch (error) {
       console.error('Error uploading files', error);
     }
