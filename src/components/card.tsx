f8b6a6ac0c6ba7ab174a2e14856c8d2320775909
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-01-14 00:46:05 +0600

    Fix render list issue in modal

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -43,6 +43,7 @@ const Card = (props) => {
     } catch (error) {
       console.error('Error uploading files', error);
     }
+    getFiles()
   };
 
   useEffect(() => {
@@ -81,16 +82,23 @@ const Card = (props) => {
           >
             <img style={{position: "absolute", top: "1rem", right: "1rem", width: "3rem", height: "3rem"}} src={Cross} alt="cross" onClick={toggleModal} />
 
+            <RowContainer style={{margin: "2.5rem"}}>
+              <input onChange={handleFileChange} type="file" multiple />
+              <UploadBtn onClick={handleUpload}> Upload</UploadBtn>
+            </RowContainer>
+
+            <div style={{margin: "1rem", display: "flex", flexDirection: "column"}}>
+
             {fileLists.map((d, i) => {
               return (
-               <>
-                 <p>{d}</p>
-               </>
+               <div key={i}>
+                 <p >{d} </p>
+               </div>
               )
             })}
+            </div>
 
-              <input onChange={handleFileChange} type="file" multiple />
-              <button onClick={handleUpload}> Upload</button>
+
           </Modal>
 
           <CardContainer>
@@ -200,5 +208,63 @@ flex-direction: row;
 align-items: center;
 p {
 font-size: 0.9rem;
+}
+`
+
+const UploadBtn = styled.button`
+  align-items: center;
+  background-color: #fee6e3;
+  border: 2px solid #111;
+  border-radius: 8px;
+  box-sizing: border-box;
+  color: #111;
+  cursor: pointer;
+  display: flex;
+  font-family: Inter,sans-serif;
+  font-size: 16px;
+  height: 48px;
+  justify-content: center;
+  line-height: 24px;
+  max-width: 100%;
+  padding: 0 25px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+
+&:after {
+  background-color: #111;
+  border-radius: 8px;
+  content: "";
+  display: block;
+  height: 48px;
+  left: 0;
+  width: 100%;
+  position: absolute;
+  top: -2px;
+  transform: translate(8px, 8px);
+  transition: transform .2s ease-out;
+  z-index: -1;
+}
+
+&:hover:after {
+  transform: translate(0, 0);
+}
+
+&:active {
+  background-color: #ffdeda;
+  outline: 0;
+}
+
+&:hover {
+  outline: 0;
+}
+
+@media (min-width: 768px) {
+  & {
+    padding: 0 40px;
+  }
 }
 `
