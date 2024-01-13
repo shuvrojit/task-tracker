f4c353282f3c0dae5f7f5e513fb09606b9c198d7
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-01-13 18:26:19 +0600

    Add data and column component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
+import { useState } from "react"
+import Column from "./components/Column"
 import Card from "./components/card"
+import data from "../data"
+import styled from "styled-components"
 
 function App() {
+  const filteredData = []
+  const upcoming = data.filter((d) => d.status === "Upcoming")
+  const incomplete = data.filter((d) => d.status === "Incomplete")
+  const todo = data.filter((d) => d.status === "To Do")
+
+  function pushFiltered(title: string, data ) {
+  filteredData.push({
+    title: `${title}`,
+    data: data,
+  })
+  }
+
+  pushFiltered("Upcoming", upcoming)
+  pushFiltered("Incomplete", incomplete)
+  pushFiltered("To Do", todo)
+
 
   return (
     <>
-      <Card />
+      <MainContainer>
+
+      {
+        filteredData.map((d, i) => {
+          return (
+            <Column props={d} key={i}/>
+          )
+
+        })
+      }
+        </MainContainer>
     </>
   )
 }
 
 export default App
+
+const MainContainer = styled.div`
+display: flex;
+flex-direction: row;
+`
